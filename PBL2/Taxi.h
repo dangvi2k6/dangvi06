1de0bcd21c8bfc168054ea413b9d0f79dfdedccc
Vi <dangvi2k6@gmail.com>
2025-10-21 09:59:43 +0700

    Add files via upload

---
+++ PBL2/Taxi.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Taxi
+{
+    string bienSo;
+    int suaChua;
+    vector<string> dsTaixe;
+    public:
+        Taxi();
+        ~Taxi();
+};
---
+++ PBL2/toaDo.h
@@ -0,0 +1,7 @@
+class ToaDo 
+{
+    int x, y;
+    ToaDo(int x=0, int y=0): x(x), y(y) {}
+    public:
+        ToaDo(int x=0, int y=0); 
+};
