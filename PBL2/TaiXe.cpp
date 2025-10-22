0d8db270b452c887378ac527a5df2b1b4fc2bda4
Nguyen Hoa Viet Xo <xonguyen2804@gmail.com>
2025-10-22 20:59:03 +0700

    Update TaiXe.cpp

---
+++ PBL2/TaiXe.cpp
@@ -0,0 +1,5 @@
+#include "TaiXe.h"
+
+TaiXe(const string& id = "", const string& ten = "", const string& ns = "",
+          const string& dt = "", const string& bl = "", bool tt = true)
+        : ID(id), tenTaiXe(ten), ngaySinh(ns), sdt(dt), bangLai(bl), trangThai(tt) {}
